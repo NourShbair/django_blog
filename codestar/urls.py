77e359e11f9d43995c1c5887e4beea0a8c3000c5
Nour <nshbair@gmail.com>
2025-02-14 15:27:47 +0000

    Add blog to path

--- codestar/urls.py
+++ codestar/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from blog import views as index_views
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('blog/', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]

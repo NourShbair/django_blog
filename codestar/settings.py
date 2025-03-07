38221bd0cd08263456ca7bf38bb63c72356faaba
Nour <nshbair@gmail.com>
2025-03-07 12:40:10 +0000

    Complete post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m+1^sz1p_b=m5ki(kn1)377-&jij=+5$_+g3ta&joo@dvib=j&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 

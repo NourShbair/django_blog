dce0df3703a5f5b1f0c850447db46da60b4c0b7a
Nour <nshbair@gmail.com>
2025-03-11 23:18:49 +0000

    Customize the generic views

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,6 @@ from django.views import generic
 from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    print(queryset)
     template_name = "post_list.html"
